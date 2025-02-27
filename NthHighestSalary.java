0ab581d1ef0628b1bda8b534876d5e346549fa52
ShineAtGit <132599040+ShineAtGit@users.noreply.github.com>
2025-02-27 15:56:00 +0530

    Update NthHighestSalary.java

--- NthHighestSalary.java
+++ NthHighestSalary.java
@@ -5,7 +5,8 @@ import java.util.Comparator;
 import java.util.Optional;
 
 public class NthHighestSalary {
-
+	//write a program to find nth highest salary in the given array.
+	//if we want to find the second highest salary, pass n=2, then you should get 78999.
 	public static void main(String[] args) {
 
 		int[] numbers = { 12340, 43214, 65436, 98749, 23443, 54443, 78999 };
